4ec947e9fa28f4a67a1c7df7fd9c0f2d42f4b3a6
Justin Esposito <jjespo27@gmail.com>
2020-12-07 00:07:17 -0500

    Add hashPassword function

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, session
+from hashlib import pbkdf2_hmac
+
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
@@ -10,7 +12,7 @@ def main():
 def handleData():
     username = request.form.get('username')
     password = request.form.get('password')
-    # password = hashPassword(password)
+    password = hashPassword(password)
     # if(authenticate(username, password)):
     session['username'] = username
     return redirect(url_for('submitVote'))
@@ -28,3 +30,11 @@ def handleVote():
     # vote(hashVote(vote), username)
     # session.clear()
     return "{0} voted for: {1}".format(username, vote)
+
+def hashPassword(pw):
+    #assuming for this project a constant salt, not secure in the real world
+    salt = b'e82feccefa6ff6521c5c0daf5d225cc5'
+    
+    #use pbkdf2 function to provide secure password hashing
+    encrypted = pbkdf2_hmac('sha256', pw.encode('utf-8'), salt, 10000)
+    return encrypted
